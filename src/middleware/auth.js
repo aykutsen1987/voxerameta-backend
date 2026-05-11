0e02ca2918ad5dbb932d329d36c0f678beff2ce5
aykutsen1987 <aykutsen1987@gmail.com>
2026-05-11 14:34:11 +0300

    Delete src directory

--- src/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-// middleware/auth.js
-// API_AUTH_TOKEN kontrolü kaldırıldı — endpoint herkese açık
-const authMiddleware = (req, res, next) => {
-  next();
-};
-module.exports = { authMiddleware };
